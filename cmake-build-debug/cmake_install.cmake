51dd114fd6b7e5aacc5eb9f4c411a7d6f57d96f7
Jose Eduardo Martinez Lara <martinezlara_joseeduardo@hotmail.com>
2020-12-08 23:40:19 -0600

    cmake build ignored

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/administaff/Desktop/cpp-code
+# Install script for directory: /Users/eduardo/Desktop/cpp-course
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/administaff/Desktop/cpp-code/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/eduardo/Desktop/cpp-course/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
