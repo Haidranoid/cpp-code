9649a8e9511c089e7a773c33e741ffc2870e83ca
Jose Eduardo Martinez Lara <martinezlara_joseeduardo@hotmail.com>
2020-12-28 01:35:06 -0600

    static cast completed

--- main.cpp
+++ main.cpp
@@ -2,7 +2,46 @@
 
 using namespace std;
 
+class Parent {
+public:
+    void speak() {
+        cout << "parent speaking" << endl;
+    }
+};
+
+class Son : public Parent {
+
+};
+
+class Daughter : public Parent {
+
+};
+
+void static_casts() {
+
+    Parent parent;
+    Son son;
+    Daughter daughter;
+
+    Parent *ptr_parent = &son;
+//    Son *ptr_son = static_cast<Son*>(&parent);
+
+    Son *ptr_son = static_cast<Son *>(ptr_parent);
+    cout << ptr_son << endl;
+
+    Parent &&p = Parent();
+    Parent &&p2 = static_cast<Parent &&>(parent);
+    p2.speak();
+
+/*  float value = 3.23;
+    cout << (int) value << endl;
+    cout << int(value) << endl;
+    cout << static_cast<int>(value) << endl;*/
+
+}
+
 int main() {
+//    static_casts();
 
     return 0;
 }
