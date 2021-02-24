9fd4a98838157549ed204c6b0fe1f4f6fecdbb27
Jose Eduardo Martinez Lara <martinezlara_joseeduardo@hotmail.com>
2021-02-24 11:58:10 -0600

    ready for another course

--- cpp-beyond/code/challenge_deque.cpp
+++ cpp-beyond/code/challenge_deque.cpp
@@ -39,6 +39,43 @@ bool is_palindrome(const string &s) {
     return true;
 }
 
+bool is_palindrome2(const string &s) {
+
+    deque<char> deq;
+    for (char c:s)
+        if (isalpha(c))
+            deq.push_back(toupper(c));
+
+
+    auto it1 = deq.begin();
+    auto it2 = deq.rbegin();
+
+    while (it1 != deq.end()) {
+        if (*it1 != *it2)
+            return false;
+
+        it1++, it2++;
+    }
+
+    return true;
+}
+
+ bool is_palindrome3(const string &s) {
+    string string_clean;
+    for (char c:s)
+        if (isalpha(c))
+            string_clean.push_back(toupper(c));
+
+    string string_reversed;
+    for (auto it = string_clean.rbegin(); it != string_clean.rend() ; it++)
+        string_reversed.push_back(*it);
+
+    if (string_clean != string_reversed)
+        return false;
+
+    return true;
+}
+
 int main() {
     std::vector<string> test_strings{"a", "aa", "aba", "abba", "abbcbba", "ab", "abc", "radar", "bob", "ana",
                                      "avid diva", "Amore, Roma", "A Toyota's a toyota", "A Santa at NASA", "C++",
--- main.cpp
+++ main.cpp
@@ -1,24 +1,8 @@
 #include <iostream>
-#include "classes/MyString/MyString.h"
 
 using namespace std;
 
 int main() {
-    cout << boolalpha;
-
-    MyString larry{"Larry"};
-    MyString moe{"Moe"};
-    MyString stooge = larry;
-
-    cout << (larry == moe) << endl;
-    cout << (larry == stooge) << endl;
-
-    cout << larry << endl;
-    MyString larry2 = -larry;
-    cout << larry2 << endl;
-
-    MyString stooges = larry + " Moe";
-    cout << stooges << endl;
 
     return 0;
 }
