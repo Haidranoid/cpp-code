f8955c4b3a281ed16db6235d16f0661a1bc261de
Martinez Lara Jose Eduardo <martinezlara_joseeduardo@hotmail.com>
2021-03-14 02:49:09 -0600

    BinaryTree delete completed - bug: when has to remove a node with 1 child with children within

--- main.cpp
+++ main.cpp
@@ -5,8 +5,7 @@ using namespace std;
 class BinaryTreeNode {
 private:
     int value;
-    BinaryTreeNode *left_node;
-    BinaryTreeNode *right_node;
+    BinaryTreeNode *left_node, *right_node;
 public:
     BinaryTreeNode(int value = 0, BinaryTreeNode *left_node = nullptr, BinaryTreeNode *right_node = nullptr)
             : value{value}, left_node{left_node}, right_node{right_node} {
@@ -52,16 +51,16 @@ public:
         }
 
         BinaryTreeNode *parent = root;
-        BinaryTreeNode *current = root;
+        BinaryTreeNode *child = root;
 
-        while (current != nullptr) {
-            parent = current;
+        while (child != nullptr) {
+            parent = child;
 
-            if (value < current->get_value())
-                current = current->get_left();
+            if (value < child->get_value())
+                child = child->get_left();
 
-            else if (value > current->get_value())
-                current = current->get_right();
+            else if (value > child->get_value())
+                child = child->get_right();
 
             else  // already exists in the tree
                 return false;
@@ -76,20 +75,20 @@ public:
     }
 
     BinaryTreeNode *search(int value) {
-        BinaryTreeNode *current = root;
+        BinaryTreeNode *child = root;
 
-        while (current != nullptr) {
-            if (value < current->get_value())
-                current = current->get_left();
+        while (child != nullptr) {
+            if (value < child->get_value())
+                child = child->get_left();
 
-            else if (value > current->get_value())
-                current = current->get_right();
+            else if (value > child->get_value())
+                child = child->get_right();
 
             else
-                return current;
+                return child;
         }
 
-        return current; //nullptr
+        return child; //nullptr
     }
 
     bool remove(int value) {
@@ -97,25 +96,78 @@ public:
             return false;
 
         BinaryTreeNode *parent = root;
-        BinaryTreeNode *current = root;
+        BinaryTreeNode *child = root;
 
-        while (current->get_value() != value) {
-            parent = current;
+        while (child->get_value() != value) {
+            parent = child;
 
-            if (value < current->get_value()) {
-                current = current->get_left();
-            } else if (value > current->get_value()) {
-                current = current->get_right();
-            }
+            if (value < child->get_value())
+                child = child->get_left();
 
-            if (current == nullptr)
+            else if (value > child->get_value())
+                child = child->get_right();
+
+            if (child == nullptr) // the value searched was not found
                 return false;
         }
 
+        // case 0 - delete root
+        if (child == root)
+            root = nullptr;
+
+
         // case 1 - remove a leaf
+        if (child->get_left() == nullptr && child->get_right() == nullptr) {
+
+            if (child->get_value() < parent->get_value())
+                parent->set_left(nullptr);
+            else
+                parent->set_right(nullptr);
+
+            return true;
+        }
+
+        // case 2 - remove child with 1 child
+        if (child->get_left() != nullptr && child->get_right() == nullptr) {
+            int temp = child->get_left()->get_value();
+            child->set_left(nullptr);
+            child->set_value(temp);
+
+            return true;
+
+        } else if (child->get_right() != nullptr && child->get_left() == nullptr) {
+            int temp = child->get_right()->get_value();
+            child->set_right(nullptr);
+            child->set_value(temp);
+
+            return true;
+        }
+
+        // case 3 - remove child with 2 child (biggest left hand)
+        if (child->get_left() != nullptr && child->get_right() != nullptr) {
+            BinaryTreeNode *parent_left_hand = root->get_left();
+            BinaryTreeNode *greatest_left_hand = parent_left_hand;
+
+            while (greatest_left_hand->get_right() != nullptr) {
+                parent_left_hand = greatest_left_hand;
+                greatest_left_hand = greatest_left_hand->get_right();
+            }
+
+            if (greatest_left_hand->get_left() != nullptr) {
+                child->set_value(greatest_left_hand->get_value());
+                greatest_left_hand->set_value(greatest_left_hand->get_left()->get_value());
+                greatest_left_hand->set_left(nullptr);
+            } else {
+                child->set_value(greatest_left_hand->get_value());
+                parent_left_hand->set_right(nullptr);
+            }
+
+            return true;
+        }
 
         return false;
     }
+
 
     void print();
 
@@ -125,19 +177,26 @@ int main() {
     auto root = new BinaryTree();
 
     root->insert(20);
-    root->insert(10);
+    root->insert(15);
     root->insert(30);
 
-    root->insert(35);
-    root->insert(32);
-    root->insert(28);
-    root->insert(29);
+    root->insert(10);
+    root->insert(17);
 
-    auto node = root->search(28);
+    root->insert(5);
+    root->insert(8);
+    root->insert(16);
+    root->insert(19);
+
+    root->insert(18);
+
+/*    auto node = root->search(28);
     if (node == nullptr)
         cout << "number did not find" << endl;
     else
-        cout << "node found: " << node << endl;
+        cout << "node found: " << node << endl;*/
+
+    root->remove(17);
 
     return 0;
 }
