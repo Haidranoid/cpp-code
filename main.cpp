b5fecb7ed4b9f878d6f338cfbbe592fc7371513f
Jose Eduardo Martinez Lara <martinezlara_joseeduardo@hotmail.com>
2021-03-16 17:07:09 -0600

    BinaryTree delete started

--- main.cpp
+++ main.cpp
@@ -1,4 +1,47 @@
 #include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+class TrieNode {
+private:
+    map<char, TrieNode *> table;
+    bool is_word;
+public:
+    TrieNode() {
+        is_word = false;
+    }
+
+    map<char, TrieNode *> get_table() {
+        return table;
+    }
+
+    bool get_is_word() {
+        return is_word;
+    }
+
+    void set_is_word(bool state) {
+        this->is_word = state;
+    }
+};
+
+class Trie {
+private:
+    TrieNode *root;
+public:
+    Trie() {
+        root = new TrieNode();
+    }
+
+    bool insert(string word) {
+        auto current_node = root;
+
+        for (char c:word) {
+            current_node->get_table()[c] = new TrieNode();
+        }
+    }
+};
 
 int main() {
 
